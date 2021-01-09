b12ed15a9060e4f36f55699a27f4d43f5911279f
napi0415 <napi0415@gmail.com>
2021-01-09 10:50:25 +0900

    add sql

--- gsacf_d07_14.sql
+++ gsacf_d07_14.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost
--- 生成日時: 2020 年 12 月 26 日 04:42
+-- 生成日時: 2021 年 1 月 09 日 02:46
 -- サーバのバージョン： 10.4.17-MariaDB
 -- PHP のバージョン: 8.0.0
 
@@ -24,48 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `asg`
---
-
-CREATE TABLE `asg` (
-  `id` int(11) NOT NULL,
-  `player_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name_kana` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `player_position` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `uni_number` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `height` int(11) NOT NULL,
-  `p_weight` int(11) NOT NULL,
-  `league` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `team` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `area` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `throws` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `bats` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `run_1` int(10) NOT NULL,
-  `run_2` int(10) NOT NULL,
-  `ball_speed_1` int(20) NOT NULL,
-  `ball_speed_2` int(20) NOT NULL,
-  `ball_speed_set_1` int(20) NOT NULL,
-  `ball_speed_set_2` int(20) NOT NULL,
-  `second_throw_1` int(20) NOT NULL,
-  `second_throw_2` int(20) NOT NULL,
-  `second_throw_3` int(20) NOT NULL,
-  `note` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- テーブルのデータのダンプ `asg`
---
-
-INSERT INTO `asg` (`id`, `player_name`, `name_kana`, `player_position`, `uni_number`, `height`, `p_weight`, `league`, `team`, `area`, `throws`, `bats`, `run_1`, `run_2`, `ball_speed_1`, `ball_speed_2`, `ball_speed_set_1`, `ball_speed_set_2`, `second_throw_1`, `second_throw_2`, `second_throw_3`, `note`) VALUES
-(10, '後藤 英行', 'ゴトウ　ヒデユキ', '投手', '18', 170, 80, 'ボーイズ', '南宇和クラブ', '愛媛', '右', '右', 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(11, '後藤 英行', 'ゴトウ　ヒデユキ', '投手', '18', 170, 80, 'ボーイズ', '南宇和クラブ', '愛媛', '右', '右', 4, 4, 0, 0, 0, 0, 0, 0, 0, ''),
-(12, '後藤 英行', 'ゴトウ　ヒデユキ', '投手', '18', 170, 80, 'ボーイズ', '南宇和クラブ', '愛媛', '右', '右', 4, 4, 130, 126, 130, 124, 0, 0, 0, ''),
-(13, '後藤 英行', 'ゴトウ　ヒデユキ', '投手', '18', 170, 80, 'ボーイズ', '南宇和クラブ', '愛媛', '右', '右', 4, 4, 130, 126, 130, 124, 0, 0, 0, 'なし'),
-(14, '鈴木　唯斗', 'スズキ　ユイト', '投手', '17', 175, 70, 'ヤング', 'SASUKE名古屋ヤング', '名古屋', '左', '左', 4, 4, 126, 126, 126, 126, 0, 0, 0, 'なし');
-
--- --------------------------------------------------------
-
---
 -- テーブルの構造 `todo_table`
 --
 
@@ -82,15 +40,7 @@ CREATE TABLE `todo_table` (
 --
 
 INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated_at`) VALUES
-(1, 'やま', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:19:51'),
-(2, 'かわ', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:20:21'),
-(3, 'くも', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:20:37'),
-(4, 'ひと', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:20:56'),
-(5, 'いぬ', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:21:12'),
-(6, 'ねこ', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:21:27'),
-(7, 'きじ', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:21:55'),
-(8, 'さる', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:22:10'),
-(9, 'おおかみ', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:22:27'),
+(9, 'おおかみsuper', '2021-01-09', '2020-12-19 00:00:00', '2021-01-08 23:20:56'),
 (10, 'ぞう', '2020-12-19', '2020-12-19 00:00:00', '2020-12-19 16:22:42'),
 (11, 'kikiki', '2020-12-12', '2020-12-19 17:25:07', '2020-12-19 17:25:07'),
 (12, 'gigigig', '2020-12-18', '2020-12-19 17:39:11', '2020-12-19 17:39:11'),
@@ -101,7 +51,34 @@ INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated_at`) 
 (17, 'もちつき', '2020-12-29', '2020-12-24 05:13:55', '2020-12-24 05:13:55'),
 (18, '大掃除', '2020-12-30', '2020-12-24 05:14:18', '2020-12-24 05:14:18'),
 (19, '年越し', '2020-12-31', '2020-12-24 05:14:27', '2020-12-24 05:14:27'),
-(20, 'walking', '2020-12-12', '2020-12-25 14:18:59', '2020-12-25 14:18:59');
+(20, 'walking', '2020-12-12', '2020-12-25 14:18:59', '2020-12-25 14:18:59'),
+(21, '宿題', '2020-12-26', '2020-12-26 15:07:46', '2020-12-26 15:07:46'),
+(22, '買い物', '2020-12-27', '2020-12-26 15:09:20', '2020-12-26 15:09:20'),
+(23, '荷物運び', '2020-12-28', '2020-12-26 15:53:48', '2020-12-26 15:53:48');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users_table`
+--
+
+CREATE TABLE `users_table` (
+  `id` int(12) NOT NULL,
+  `username` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_admin` int(1) NOT NULL,
+  `is_deleted` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `users_table`
+--
+
+INSERT INTO `users_table` (`id`, `username`, `password`, `is_admin`, `is_deleted`, `created_at`, `updated_at`) VALUES
+(1, 'goto', '1919weimarer', 0, 0, '2021-01-08 23:41:40', '2021-01-08 23:41:40'),
+(2, 'demo', 'yamakawa', 0, 0, '2021-01-09 10:44:55', '2021-01-09 10:44:55');
 
 -- --------------------------------------------------------
 
@@ -165,15 +142,15 @@ INSERT INTO `wbc2018_u15_table` (`id`, `player_name`, `name_kana`, `player_posit
 --
 
 --
--- テーブルのインデックス `asg`
---
-ALTER TABLE `asg`
-  ADD PRIMARY KEY (`id`);
-
---
 -- テーブルのインデックス `todo_table`
 --
 ALTER TABLE `todo_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `users_table`
+--
+ALTER TABLE `users_table`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -187,16 +164,16 @@ ALTER TABLE `wbc2018_u15_table`
 --
 
 --
--- テーブルの AUTO_INCREMENT `asg`
---
-ALTER TABLE `asg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
 -- テーブルの AUTO_INCREMENT `todo_table`
 --
 ALTER TABLE `todo_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- テーブルの AUTO_INCREMENT `users_table`
+--
+ALTER TABLE `users_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- テーブルの AUTO_INCREMENT `wbc2018_u15_table`
